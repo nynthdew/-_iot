ef0595aa19a4cc8aaf6c95a91fc5749b9ae6753b
Nanthawat Phongsatja <84176064+nynthdew@users.noreply.github.com>
2021-06-29 15:00:19 +0700

    Update DHT1READ.ino

--- DHT1READ.ino
+++ DHT1READ.ino
@@ -1,28 +1,28 @@
 #include "DHT.h"
-#define D4 2   // TXD1
-#define DHTPIN D4     // what digital pin we're connected to
-#define DHTTYPE DHT22  // DHT 11
+#define D4 2  
+#define DHTPIN D4  
+#define DHTTYPE DHT22  
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() 
 {
   Serial.begin(115200);
-  dht.begin();  // เริ่มต้นเรียกใช้งาน Library DHT11 
+  dht.begin();  
 }
 
 void loop()
 {
-  // Wait a few seconds between measurements.
+
   delay(2000);
   float Humidity = dht.readHumidity();
-  // Read temperature as Celsius (the default)
+
   float Temp = dht.readTemperature();
 
-  // Check if any reads failed and exit early (to try again).
+
   if (isnan(Humidity) || isnan(Temp)) 
   {
-    Serial.println("Failed to read from DHT sensor!");  // บอกสถานะหากเกิดการผิดพลาดในการอ่านข้อมูล สาเหตุอาจเกิดได้จากสัญญาณรบกวน หรือการเชื่อมต่อที่ไม่สมบรูณ์
+    Serial.println("Failed to read from DHT sensor!");  
     return;
   }
   else
